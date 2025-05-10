2b4d5d04ec531a5091444435c10933c16ef4e652
John Balestrini <>
2025-05-10 10:25:49 -0700

    wip

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@ Route::get('dashboard', function () {
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 
-Route::get('/tasmota/log', [SensorDataController::class, 'store']);
+Route::post('/sensor', [SensorDataController::class, 'store']);
 
 
 
