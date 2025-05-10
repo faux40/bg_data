2d09f88255123a3451158aa6090bd51e1ada1341
John Balestrini <>
2025-05-10 10:32:52 -0700

    wip

--- routes/api.php
+++ routes/api.php
@@ -3,4 +3,5 @@
 use App\Http\Controllers\SensorDataController;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/sensor', [SensorDataController::class, 'store']);
+// Route::post('/sensor', [SensorDataController::class, 'store']);
+Route::get('/sensor', [SensorDataController::class, 'store']);
